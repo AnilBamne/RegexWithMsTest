b7fb287e66aee5cde94a94e1b4bc76a1988a1f1a
AnilBamne <anilbamne20@gmail.com>
2022-12-12 12:03:32 +0530

    Adding:WelcomeMessage

---
+++ RegexTestUserResistration/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RegexTestUserResistration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Regex Testing with Ms test");
+        }
+    }
+}
